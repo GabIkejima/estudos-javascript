58bd91b4c2d96e38135117ae8bc8a59a0505aa03
GabIkejima <118475573+GabIkejima@users.noreply.github.com>
2023-02-22 21:38:49 -0300

    Delete Projetos directory

--- Projetos/imparPar.js
+++
@@ -1,9 +0,0 @@
-const numero = 10;
-const numeroPar = (numero % 2) === 0;
-
-if(numeroPar){
-    console.log('O número é par!')
-}else //(!numeroPar)
-{                    
-    console.log('O número é ímpar')
-}
